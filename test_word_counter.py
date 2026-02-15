94c5e73056b675aae751b6ace530723a4f6021ef
Kevin Angstadt <kangstadt@stlawu.edu>
2026-02-15 16:04:06 -0500

    Add Word Counter feature

---
+++ test_word_counter.py
@@ -0,0 +1,12 @@
+import unittest
+from word_counter import count_words
+
+class TestWordCounter(unittest.TestCase):
+    def test_simple_sentence(self):
+        self.assertEqual(count_words("Hello world"), 2)
+
+    def test_single_word(self):
+        self.assertEqual(count_words("Python"), 1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ word_counter.py
@@ -0,0 +1,11 @@
+def count_words(sentence):
+    """
+    Counts the number of words in a given sentence.
+
+    Args:
+        sentence (str): The text to analyze.
+
+    Returns:
+        int: The number of words.
+    """
+    return len(sentence.split(" "))
